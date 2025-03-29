740b2ba7f0e7bb289a68cbcbcf978d50712607bf
Ahmad Hassan Khan <ahmadhassankhan701@gmail.com>
2025-03-29 16:45:01 +0300

    notes app

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDbZQfryMVhUwI1NA2MNyII8nA2jH5q8pA",
-	authDomain: "keepnotes-a65ad.firebaseapp.com",
-	projectId: "keepnotes-a65ad",
-	storageBucket: "keepnotes-a65ad.appspot.com",
-	messagingSenderId: "4178282022",
-	appId: "1:4178282022:web:5ad064224299c7720548e7",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const app = initializeApp(firebaseConfig, "keep_notes");
